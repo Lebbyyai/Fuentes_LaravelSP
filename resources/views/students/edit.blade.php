64feda7f3210beff23475e4c2bb58e64a7993dbd
Lebbyyai <lfuentes_240000000631@uic.edu.ph>
2026-01-20 10:16:16 +0800

    Updated the edit blade file

--- resources/views/students/edit.blade.php
+++ resources/views/students/edit.blade.php
@@ -1,29 +1,31 @@
 @extends('layout')
 
+@section('title', 'Edit Student')
+
 @section('content')
 <div class="row justify-content-center">
     <div class="col-md-6">
-        <div class="card">
-            <div class="card-header">
-                <h4>Edit Student</h4>
+        <div class="card shadow">
+            <div class="card-header bg-warning">
+                <h5 class="mb-0">Edit Student Details</h5>
             </div>
             <div class="card-body">
                 <form>
                     <div class="mb-3">
-                        <label>Full Name</label>
-                        <input type="text" class="form-control" value="John Doe">
+                        <label>Name</label>
+                        <input type="text" class="form-control" value="Allen Bagsakan">
                     </div>
                     <div class="mb-3">
                         <label>Email</label>
-                        <input type="email" class="form-control" value="johndoe@example.com">
+                        <input type="email" class="form-control" value="allen.bagsakan@university.edu">
                     </div>
                     <div class="mb-3">
                         <label>Course</label>
-                        <input type="text" class="form-control" value="BS Computer Science">
+                        <input type="text" class="form-control" value="BS Civil Engineering">
                     </div>
                     <div class="mb-3">
                         <label>Year Level</label>
-                        <input type="number" class="form-control" value="3">
+                        <input type="number" class="form-control" value="5">
                     </div>
                     <div class="mb-3">
                         <button type="submit" class="btn btn-success">Update</button>
